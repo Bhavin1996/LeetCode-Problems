4c7890174e4f179933bb199b1a22a4d267bce8cb
Sudhanshu <bhavin.bawane@gmail.com>
2023-09-22 21:51:57 +0530

    Update 3sum.go

--- 3sum.go
+++ 3sum.go
@@ -1,7 +1,6 @@
-package main
+package leetcode
 
 import (
-	"fmt"
 	"sort"
 )
 
@@ -45,9 +44,4 @@ func threeSum(nums []int) [][]int {
 	}
 
 	return result
-}
-
-func main() {
-	nums := []int{-1, 0, 1, 2, -1, -4}
-	fmt.Println(threeSum(nums[:]))
 }
