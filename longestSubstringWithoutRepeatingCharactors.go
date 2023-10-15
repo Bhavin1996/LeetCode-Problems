fadaea91dd7221589ad1e10ba9e8bbe0db22489c
Sudhanshu <bhavin.bawane@gmail.com>
2023-10-16 00:06:12 +0530

    Update longestSubstringWithoutRepeatingCharactors.go

--- longestSubstringWithoutRepeatingCharactors.go
+++ longestSubstringWithoutRepeatingCharactors.go
@@ -3,16 +3,20 @@ package leetcode
 func lengthOfLongestSubstring(s string) int {
 	lookup := make(map[rune]int, 26)
 	count, maxCount := 0, 0
-	for _, ch := range s {
-		count++
-		if _, isPresent := lookup[ch]; isPresent {
-			if count > maxCount {
-				maxCount = count - 1
-			}
-			count = 0
-		} else {
-			lookup[ch-'a']++
+	start := 0
+
+	for i, ch := range s {
+		if pos, isPresent := lookup[ch]; isPresent && pos >= start {
+			start = pos + 1
+		}
+
+		count = i - start + 1
+		lookup[ch] = i
+
+		if count > maxCount {
+			maxCount = count
 		}
 	}
+
 	return maxCount
 }
