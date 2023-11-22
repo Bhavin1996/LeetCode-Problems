577c09328a071c027db4366089b8d0e5d7e8d7b2
Bhavin1996 <bhavin.bawane@gmail.com>
2023-11-22 13:43:37 +0530

    new additions

--- testing.go
+++ testing.go
@@ -1,16 +1,36 @@
 package leetcode
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
 
-func add(x chan int) {
-	res := <-x + (<-x + 1)
-	fmt.Println(res)
-}
-
-func main_add() {
-	ch := make(chan int)
-	for i := 0; i < 100; i++ {
-		ch <- i
+func main_file() {
+	dir := "/home/raiden/Desktop/FILE_IO/"
+	fileDir, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println("Issue with the direcetory while accesing")
 	}
-	go add(ch)
+	re, err := regexp.Compile(`^(25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]).{3}(25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9])`)
+
+	for _, fileName := range fileDir {
+		filePath := filepath.Join(dir, fileName.Name())
+		file, err := os.ReadFile(filePath)
+
+		for _, data := range file {
+			words := strings.Fields(string(data))
+
+			for _, word := range words {
+				if re.MatchString(word) && err != nil {
+					fmt.Println("Valid IP address ", word)
+				}
+			}
+
+		}
+
+	}
+
 }
